8864e0d44268701038f50c0ffb68507ab0de563c
nipunas <nipuna77@gmail.com>
2018-11-11 11:44:22 +0530

    Super Classes

--- src/app/models/task.model.ts
+++ src/app/models/task.model.ts
@@ -6,10 +6,33 @@ export class Task {
 
 // ES6 Feature : Shorthand for Initializing Properties
 export class ExtendedTask {
-    function (name: string, description: string) {
+    function(name: string, description: string) {
         return {
             name,
             description
         };
+    }
+}
+
+export class BaseModel {
+    name: string;
+    options: {};
+    url: string;
+    data: any[];
+    constructor(options = {}, data = []) { // class constructor
+        this.name = 'Base';
+        this.url = 'http://azat.co/api';
+        this.data = data;
+        this.options = options;
+    }
+
+    getName() { // class method
+        console.log(`Class name: ${this.name}`);
+    }
+}
+
+export class AccountModel extends BaseModel {
+    constructor(options, data) {
+        super();
     }
 }
